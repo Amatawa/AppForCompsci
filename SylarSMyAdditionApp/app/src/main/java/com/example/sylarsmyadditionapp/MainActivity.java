f1bc33ae964b5c9b2ae55d91286812d3e3d326c6
Amatawa <sylar6smith@gmail.com>
2019-02-20 11:58:45 -0700

    The app beginsgit status

--- SylarSMyAdditionApp/app/src/main/java/com/example/sylarsmyadditionapp/MainActivity.java
+++ SylarSMyAdditionApp/app/src/main/java/com/example/sylarsmyadditionapp/MainActivity.java
@@ -2,6 +2,10 @@ package com.example.sylarsmyadditionapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,20 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button addBtn = (Button) findViewByID(R.id.addBtn);
+        addBtn.setOnClickListerner(new View.OnClickListener(){
+           @Override
+           public void onClick(View v) {
+               EditText firstNumEditText = findViewById(R.id.firstNumEditText);
+               EditText secondNumEditText = findViewById(R.id.secondNumEditText);
+               TextView resultTextView = findViewById(R.id.resultTextView);
+
+               double num1 = Double.parseDouble(firstNumEditText.getText().toString());
+               double num2 = Double.parseDouble(secondNumEditText.getText().toString());
+               double result = num1 + num2;
+               resultTextView.setText(result + "");
+           }
+        });
     }
 }
