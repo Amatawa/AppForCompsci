8b13dfc03a4ecb8541f3b6f73f9dccc4254c0408
Amatawa <sylar6smith@gmail.com>
2019-04-17 11:40:00 -0600

    DB project

---
+++ MySQLSampleProject/app/src/main/java/com/example/mysqlsampleproject/DbStrings.java
@@ -0,0 +1,9 @@
+package com.example.mysqlsampleproject;
+
+public class DbStrings {
+
+    static final String DATABASE_URL = "19.168.1.116";
+    static final String DATABASE_NAME = "fruit";
+    static final String USERNAME = "root";
+    static final String PASSWORD = "toor";
+}
